3c3544c9e1a8e0704d5d3100f2f192754f94de25
zack32527 <157523220+zack32527@users.noreply.github.com>
2024-04-02 16:16:36 -0400

    Zach Bryan Image Gallery

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+ if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+   document.getElementById("cornerBtn").style.display = "block";
+ } else {
+   document.getElementById("cornerBtn").style.display = "none";
+ }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+}
